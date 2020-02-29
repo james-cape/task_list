7561457a288e643c72263300e99f2b18ab3a598f
James Cape <james.cape@gmail.com>
2020-02-28 21:07:00 -0700

    Adds GET task controller method

--- app.go
+++ app.go
@@ -4,6 +4,9 @@ import (
   "database/sql"
   "fmt"
   "log"
+  "encoding/json"
+  "net/http"
+  "strconv"
 
   "github.com/gorilla/mux"
   _ "github.com/go-sql-driver/mysql"
@@ -29,3 +32,37 @@ func (a *App) Initialize(user, password, dbname string) {
 
 // Starts the application
 func (a * App) Run(addr string) { }
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+  respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+  response, _ := json.Marshal(payload)
+
+  w.Header().Set("Content-Type", "application/json")
+  w.WriteHeader(code)
+  w.Write(response)
+}
+
+func (a *App) getTask(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+  id, err := strconv.Atoi(vars["id"])
+  if err != nil {
+    respondWithError(w, http.StatusBadRequest, "Invalid task ID")
+    return
+  }
+
+  t := task{ID: id}
+  if err := t.getTask(a.DB); err != nil {
+    switch err {
+      case sql.ErrNoRows:
+        respondWithError(w, http.StatusNotFound, "Task not found")
+      default:
+        respondWithError(w, http.StatusInternalServerError, err.Error())
+      }
+      return
+  }
+
+  respondWithJSON(w, http.StatusOK, t)
+}
